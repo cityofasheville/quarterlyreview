f68986e669427adbdb075b5e7ddef97dc1934033
Eric Jackson <ejackson@ashevillenc.gov>
2017-06-19 15:23:02 -0400

    Updated the front page to include my reviews

--- QuarterlyReview/Controllers/HomeController.cs
+++ QuarterlyReview/Controllers/HomeController.cs
@@ -33,23 +33,64 @@ namespace QuarterlyReview.Controllers
         {
             if (User.Identity.IsAuthenticated)
             {
+                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
                 DateTime startDate = DateTime.Now;
                 DateTime endDate = startDate.AddDays(90);
                 string getEmployee = "EXECUTE dbo.avp_Get_Employee @UserEmpId";
                 string getMyEmployees = "EXECUTE dbo.avp_Get_My_Employees @UserEmpId";
-                ApplicationUser user = await _userManager.GetUserAsync(HttpContext.User);
-                ViewData["EmployeeID"] = user.EmployeeID;
+                string getReviewsOfMe = "EXECUTE dbo.avp_Reviews_Of_Me @UserEmpId";
+
                 var empId = new SqlParameter("@UserEmpId", user.EmployeeID);
                 var employee = await _context.Employees.FromSql(getEmployee, empId)
                     .SingleOrDefaultAsync<Employees>();
-                ViewData["EmployeeID"] = user.EmployeeID;
-                ViewData["Supervisor"] = employee.Supervisor;
-                //ViewData["Supervisor"] = String.Format("Hello {0}", employee.Supervisor);
-
                 var myEmployees = await _context.Employees.FromSql(getMyEmployees, empId)
                     .ToListAsync<Employees>();
+                List<DisplayReviewSummary> myReviews = new List<DisplayReviewSummary>();
+
+                // Read reviews of me
+
+                var getMyReviewsCmd = _context.Database.GetDbConnection().CreateCommand();
+
+                int mrid = employee.EmpId;
+                // mrid = 3399;
+                getMyReviewsCmd.CommandText = "EXEC [dbo].[avp_Reviews_of_Me] " +
+                    String.Format("@UserEmpID = {0}", mrid);
+
+                _context.Database.OpenConnection();
+                using (var result = getMyReviewsCmd.ExecuteReader())
+                {
+                    if (result.HasRows)
+                    {
+                        while (result.Read())
+                        {
+                            DisplayReviewSummary rev = new DisplayReviewSummary(
+                                result.GetInt32(0), // RT_ID
+                                result.GetInt32(1), // R_ID
+                                result.GetString(2), // RT_Name
+                                result.GetString(3), // RT_Desc
+                                result.GetString(4), // Status
+                                result.GetDateTime(5), // Status_Date
+                                result.GetInt32(6), // SupID
+                                result.GetInt32(7), // EmpID
+                                result.GetString(8), // Employee
+                                result.GetString(9), // DivID
+                                result.GetString(10), // Position
+                                result.GetDateTime(11), // PeriodStart
+                                result.GetDateTime(12) // PeriodEnd
+                             );
+                            myReviews.Add(rev);
+                        }
+                    }
+                }
+
+                // Done
+
+                ViewData["EmployeeID"] = user.EmployeeID;
+                ViewData["EmployeeID"] = user.EmployeeID;
+                ViewData["Supervisor"] = employee.Supervisor;
                 ViewData["Employees"] = myEmployees;
                 ViewData["Employee"] = employee.Employee;
+                ViewData["MyReviews"] = myReviews;
                 return View(myEmployees);
 
             }
---
+++ QuarterlyReview/Models/DisplayReviewSummary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuarterlyReview.Models
+{
+    public class DisplayReviewSummary
+    {
+        public int reviewTemplateID { get; set; }
+        public int reviewID { get; set; }
+        public string reviewTemplateName { get; set; }
+        public string reviewTemplateDescription { get; set; }
+        public string status { get; set; }
+        public DateTime statusDate { get; set; }
+        public int supervisorID { get; set; }
+        public int employeeID { get; set; }
+        public string employeeName { get; set; }
+        public string divisionID { get; set; }
+        public string position { get; set; }
+        public DateTime periodStart { get; set; }
+        public DateTime periodEnd { get; set; }
+
+        public DisplayReviewSummary(int rt_id, int r_id, string rt_name, string rt_desc, string stat,
+            DateTime stat_date, int sid, int e_id, string e_name, string div_id, string pos,
+            DateTime start, DateTime end)
+        {
+            reviewTemplateID = rt_id;
+            reviewID = r_id;
+            reviewTemplateName = rt_name;
+            reviewTemplateDescription = rt_desc;
+            status = stat;
+            statusDate = stat_date;
+            supervisorID = sid;
+            employeeID = e_id;
+            employeeName = e_name;
+            divisionID = div_id;
+            position = pos;
+            periodStart = start;
+            periodEnd = end;
+        }
+    }
+}
